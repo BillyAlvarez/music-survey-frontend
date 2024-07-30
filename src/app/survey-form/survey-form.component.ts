2cc2dc8a76fa6afcc451bc7198d8feb539480020
BillyAlvarez <billyjag1@gmail.com>
2024-07-30 04:14:06 -0400

    Add Survey-form url

--- src/app/survey-form/survey-form.component.ts
+++ src/app/survey-form/survey-form.component.ts
@@ -1,14 +1,33 @@
-import { Component } from '@angular/core';
-import {RouterLink, RouterModule} from "@angular/router";
+import {Component, inject} from '@angular/core';
+import {Router, RouterLink, RouterModule} from "@angular/router";
+import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
+import {log} from "@angular-devkit/build-angular/src/builders/ssr-dev-server";
+import {SurveysService} from "../services/surveys.service";
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
   imports: [
-    RouterLink, RouterModule],
+    RouterLink, RouterModule,ReactiveFormsModule],
   templateUrl: './survey-form.component.html',
   styleUrl: './survey-form.component.css'
 })
 export default class SurveyFormComponent {
+private fb = inject(FormBuilder);
+private router = inject(Router);
+private surveysService = inject(SurveysService);
+
+form = this.fb.group({
+  email: ['', [Validators.required]],
+  musicStyle: ['', [Validators.required]]
+});
+
+create(){
+  const body = this.form.value;
+  this.surveysService.create(body)
+    .subscribe(() => {
+this.router.navigate(['/']).then(r => {});
+    });
+}
 
 }
