58dd20e42a3258ad217da62cc71d689728da229c
BillyAlvarez <billyjag1@gmail.com>
2024-07-31 01:44:30 -0400

    Added Validations

--- src/app/survey-form/survey-form.component.ts
+++ src/app/survey-form/survey-form.component.ts
@@ -2,12 +2,13 @@ import {Component, inject} from '@angular/core';
 import {Router, RouterLink, RouterModule} from "@angular/router";
 import {FormBuilder, ReactiveFormsModule, Validators} from "@angular/forms";
 import {SurveysService} from "../services/surveys.service";
+import {CommonModule} from "@angular/common";
 
 @Component({
   selector: 'app-survey-form',
   standalone: true,
   imports: [
-    RouterLink, RouterModule, ReactiveFormsModule],
+    RouterLink, RouterModule, ReactiveFormsModule, CommonModule],
   templateUrl: './survey-form.component.html',
   styleUrl: './survey-form.component.css'
 })
@@ -15,22 +16,33 @@ export default class SurveyFormComponent {
   private fb = inject(FormBuilder);
   private router = inject(Router);
   private surveysService = inject(SurveysService);
+  successMessage: string = '';
+  errorMessage: string = '';
 
   form = this.fb.group({
     email: ['', [Validators.required, Validators.email]],
     musicStyle: ['', [Validators.required]]
   });
 
-  create() {
+  create(): void {
     if (this.form.invalid) {
       return;
     }
-    const body = this.form.value;
-    this.surveysService.create(body)
-      .subscribe(() => {
-        this.router.navigate(['/results']).then(r => {
-        });
-      });
-  }
 
+    const body = this.form.value;
+    this.surveysService.create(body).subscribe(
+      () => {
+        this.successMessage = '¡Encuesta guardada exitosamente!';
+        this.errorMessage = '';
+        setTimeout(() => {
+          this.router.navigate(['/results']);
+        }, 1000);
+      },
+      error => {
+        this.errorMessage = 'Hubo un error al guardar la encuesta, El Email ya fue registrado.';
+        this.successMessage = '';
+        this.form.reset();
+      }
+    );
+  }
 }
